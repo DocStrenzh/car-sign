a4d7b000947ecc78d3c8c91c6d5efbdb7db96a98
DocStrenzh <mishaprokhorov2014@yandex.ru>
2024-10-30 15:16:24 +0300

    Fixed the logo overlay

--- car-number.py
+++ car-number.py
@@ -2,7 +2,7 @@ from inference_sdk import InferenceHTTPClient
 import cv2
 import numpy as np
 
-def add_overlay_only(car_image_path, output_image_path, logo_path):
+def add_rotated_logo_with_overlay(car_image_path, logo_image_path, output_image_path):
     CLIENT = InferenceHTTPClient(
         api_url="https://outline.roboflow.com",
         api_key="mb7m4Bcuxtbp5gAuPR0j"
@@ -15,13 +15,9 @@ def add_overlay_only(car_image_path, output_image_path, logo_path):
         return
 
     car_image = cv2.imread(car_image_path)
-    if car_image is None:
-        print("Error loading car image.")
-        return
-
-    logo = cv2.imread(logo_path, cv2.IMREAD_UNCHANGED)
-    if logo is None:
-        print("Error loading logo image.")
+    logo_image = cv2.imread(logo_image_path, cv2.IMREAD_UNCHANGED)
+    if car_image is None or logo_image is None:
+        print("Error loading images.")
         return
 
     for pred in predictions:
@@ -36,15 +32,20 @@ def add_overlay_only(car_image_path, output_image_path, logo_path):
             box = cv2.boxPoints(rect)
             box = np.int0(box)
 
-            src_points = np.float32(box)
+            src_points = np.float32([
+                box[1],
+                box[2],
+                box[3],
+                box[0]
+            ])
 
-            overlay_w, overlay_h = int(rect[1][0]), int(rect[1][1])
-            white_overlay = np.ones((overlay_h, overlay_w, 3), dtype=np.uint8) * 255
+            h, w = car_image.shape[:2]
+            white_overlay = np.ones((h, w, 3), dtype=np.uint8) * 255
             dst_points_overlay = np.float32([
                 [0, 0],
-                [overlay_w, 0],
-                [overlay_w, overlay_h],
-                [0, overlay_h]
+                [w, 0],
+                [w, h],
+                [0, h]
             ])
 
             matrix_overlay = cv2.getPerspectiveTransform(dst_points_overlay, src_points)
@@ -55,27 +56,32 @@ def add_overlay_only(car_image_path, output_image_path, logo_path):
             car_image = cv2.bitwise_and(car_image, car_image, mask=cv2.bitwise_not(mask_overlay))
             car_image = cv2.add(car_image, warped_overlay)
 
-            logo_resized = cv2.resize(logo, (overlay_w, overlay_h), interpolation=cv2.INTER_AREA)
+            logo_h, logo_w = logo_image.shape[:2]
+            dst_points_logo = np.float32([
+                [0, 0],
+                [logo_w, 0],
+                [logo_w, logo_h],
+                [0, logo_h]
+            ])
 
-            if logo_resized.shape[2] == 4:
-                logo_mask = logo_resized[:, :, 3]
-                logo_bgr = logo_resized[:, :, :3]
+            matrix_logo = cv2.getPerspectiveTransform(dst_points_logo, src_points)
+            warped_logo = cv2.warpPerspective(logo_image, matrix_logo, (car_image.shape[1], car_image.shape[0]))
+
+            if warped_logo.shape[2] == 4:
+                alpha_mask = warped_logo[:, :, 3] / 255.0
+                for c in range(3):
+                    car_image[:, :, c] = (1 - alpha_mask) * car_image[:, :, c] + alpha_mask * warped_logo[:, :, c]
             else:
-                logo_mask = np.ones((overlay_h, overlay_w), dtype=np.uint8) * 255
-                logo_bgr = logo_resized
-
-            roi = car_image[int(points[0][1]):int(points[0][1]) + overlay_h, int(points[0][0]):int(points[0][0]) + overlay_w]
-
-            for c in range(0, 3):
-                roi[:, :, c] = roi[:, :, c] * (1 - logo_mask / 255.0) + logo_bgr[:, :, c] * (logo_mask / 255.0)
-
-            car_image[int(points[0][1]):int(points[0][1]) + overlay_h, int(points[0][0]):int(points[0][0]) + overlay_w] = roi
+                mask_logo = cv2.cvtColor(warped_logo, cv2.COLOR_BGR2GRAY)
+                _, mask_logo = cv2.threshold(mask_logo, 1, 255, cv2.THRESH_BINARY)
+                car_image = cv2.bitwise_and(car_image, car_image, mask=cv2.bitwise_not(mask_logo))
+                car_image = cv2.add(car_image, warped_logo)
 
     cv2.imwrite(output_image_path, car_image)
     print(f"Output image saved at {output_image_path}")
 
-add_overlay_only(
+add_rotated_logo_with_overlay(
     car_image_path='test-images/car_3.png',
-    output_image_path='output/cars_with_logo_overlay.png',
-    logo_path='logo/Logo.png'
+    logo_image_path='logo/Logo.png',
+    output_image_path='output/cars_with_rotated_logo_and_overlay.png'
 )
